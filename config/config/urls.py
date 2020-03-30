3e11eb4790308b690557dc15261c74e7807d4342
arty <juicehq@yandex.ru>
2020-03-30 10:10:33 +0300

    add new /index.html

--- config/config/urls.py
+++ config/config/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from .views import start
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -26,4 +27,6 @@ urlpatterns = [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', start),
+
 ]
--- config/promise/urls.py
+++ config/promise/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import PromiseCreateView, PromiseDetailView, PromiseListView, start
+from .views import PromiseCreateView, PromiseDetailView, PromiseListView
 from django.conf.urls import include
 from django.urls import path
 
@@ -14,7 +14,6 @@ urlpatterns = [
     # url(r'^promise/', PromiseCreateView.as_view()),
     url(r'^promise/$', PromiseListView.as_view()),
     path('promise/<str:slug>/', PromiseDetailView.as_view()),
-    path('', start),
 
     # url(r'^promise/name/(?P<slug>[A-Za-z]+.)/$', PromiseDetailView.as_view()),
     # path('promise/users/detail/<str:nickname>/', UserInfoDetailView.as_view()),
