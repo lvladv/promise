db652ecd7919a5656aa89f80f0683c3d501e217e
arty <juicehq@yandex.ru>
2020-04-04 15:07:52 +0300

    create service.properties change address in tests

--- config/config/urls.py
+++ config/config/urls.py
@@ -8,7 +8,7 @@ from .views import start
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Pomise API",
         default_version='v1',
         description="Test description",
         # terms_of_service="https://www.google.com/policies/terms/",
