b911fee9bc8b2bf3fb0ccf37432081f97405940d
arty <juicehq@yandex.ru>
2020-04-15 21:18:22 +0300

    1)change method for users visible 2) add deadline_row for parse timestamp deadline

--- config/promise/models.py
+++ config/promise/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from uuid import uuid4
 # from config.userdetail.models import User
 from userdetail.models import User
+from datetime import datetime, timedelta
+from django.utils.timezone import make_aware
 from django.utils.text import slugify
 from autoslug import AutoSlugField
 from autoslug.settings import slugify as default_slugify
@@ -9,12 +11,25 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+def create_deadline(row):
+    delta = row.split(' ')
+    num = int(delta[0])
+    type = delta[1]
+    if type == 'min':
+        return datetime.now() + timedelta(minutes=num)
+    elif type == 'hour':
+        return datetime.now() + timedelta(hours=num)
+    elif type == 'day':
+        return datetime.now() + timedelta(days=num)
+
+
 class Promise(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=False)
     slug = models.SlugField(unique=True, blank=True, default=None)
     description = models.CharField(max_length=2024, blank=False, default='')
-    deadline = models.CharField(max_length=2024, blank=False, default='60')
+    deadline_row = models.CharField(max_length=255, blank=True)
+    deadline = models.DateTimeField(blank=True, default=None)
     status = models.CharField(max_length=1, blank=False, default='D')
     is_approved = models.BooleanField(default=False)
     modify_time = models.DateTimeField(auto_now=True)
@@ -25,6 +40,7 @@ class Promise(models.Model):
         if self.slug is None:
             prefix = self.owner.__str__()
             self.slug = prefix + '-' + str(uuid4())[:8]
+        self.deadline = make_aware(create_deadline(self.deadline_row))
         super(Promise, self).save(*args, **kwargs)
 
     REQUIRED_FIELDS = ['name', 'description', 'deadline', 'status']
