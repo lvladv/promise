1df35dd7167b935c9da64f4b8bfb4569610de4c6
lvladv <mr.lvladv@yandex.ru>
2020-05-17 12:42:28 +0300

    build list change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.scss";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { rootReducer } from "./store/reducersCombaine";
 
 const store = createStore(
   rootReducer,
-  compose(
+  // compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // )
 );
 
 ReactDOM.render(
