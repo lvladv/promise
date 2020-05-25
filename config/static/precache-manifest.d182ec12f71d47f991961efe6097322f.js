1dc845595e3b190270a235c44aab94070c9a1136
lvladv <mr.lvladv@yandex.ru>
2020-05-25 22:04:16 +0300

    new url case

---
+++ config/static/precache-manifest.d182ec12f71d47f991961efe6097322f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b749b38ec7291e2379c455617503270a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d7b4a8accf4c8739d801",
+    "url": "./static/css/main.5ac0a38c.chunk.css"
+  },
+  {
+    "revision": "25ee51fc156594a81d34",
+    "url": "./static/js/2.5f4f5c70.chunk.js"
+  },
+  {
+    "revision": "fbb6e99068dfb0db4c7511bdc3c130cc",
+    "url": "./static/js/2.5f4f5c70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7b4a8accf4c8739d801",
+    "url": "./static/js/main.7df449d6.chunk.js"
+  },
+  {
+    "revision": "77cb8304b1f6c5dea065",
+    "url": "./static/js/runtime-main.3094a97f.js"
+  }
+]);
