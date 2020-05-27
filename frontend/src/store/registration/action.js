9a00566265ae784f986b5bd1a810b45343c3f9aa
lvladv <mr.lvladv@yandex.ru>
2020-05-27 18:40:22 +0300

    refact errors, npm package

--- frontend/src/store/registration/action.js
+++ frontend/src/store/registration/action.js
@@ -1,5 +1,4 @@
-import isEmail from "validator/lib/isEmail";
-import {url} from "./../url"
+import { url } from "./../url";
 export const REGISTRATION = "REGISTRATION";
 export const INPUT_ERROR = "INPUT_ERROR";
 export const ERROR = "ERROR";
@@ -8,47 +7,34 @@ export const OK_REGISTRATION = "OK_REGISTRATION";
 
 export const newUser = (newEmail, newUsername, newPassword) => {
   return async (dispatch) => {
-    if (
-      newEmail !== "" &&
-      isEmail(newEmail) &&
-      newUsername !== "" &&
-      newPassword !== "" &&
-      newPassword.length > 8 &&
-      (newEmail !== newUsername) !== newPassword
-    ) {
-      let formData = new FormData();
-      formData.append("username", newUsername);
-      formData.append("password", newPassword);
-      formData.append("email", newEmail);
+    let formData = new FormData();
+    formData.append("username", newUsername);
+    formData.append("password", newPassword);
+    formData.append("email", newEmail);
 
-      let requestOptions = {
-        method: "POST",
-        body: formData,
-      };
+    let requestOptions = {
+      method: "POST",
+      body: formData,
+    };
 
-      let response = await fetch(
-        `http://${url}/api/v1/data/auth/users/`,
-        requestOptions
-      );
-      let autor = await response.json();
+    let response = await fetch(
+      `http://${url}/api/v1/data/auth/users/`,
+      requestOptions
+    );
+    let autor = await response.json();
 
-      if (response.ok) {
-        await dispatch({
-          type: OK_REGISTRATION,
-        });
-      } else if (!response.ok) {
-        await dispatch({
-          type: ERROR,
-          login: autor.username,
-          email: autor.email,
-          password: autor.password,
-        });
-        console.log(autor);
-      }
-    } else {
+    if (response.ok) {
       await dispatch({
-        type: INPUT_ERROR,
+        type: OK_REGISTRATION,
       });
+    } else if (!response.ok) {
+      await dispatch({
+        type: ERROR,
+        login: autor.username,
+        email: autor.email,
+        password: autor.password,
+      });
+      console.log(autor);
     }
   };
 };
